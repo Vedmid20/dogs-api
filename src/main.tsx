b8774efad918365bc4f1dd5c1c2029341fa33aad
Vedmid20 <lysyy.boba.freelance@gmail.com>
2024-12-16 13:31:25 +0200

    React + Redux for work with API

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './store/store';
+import { store, persistor } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <Provider store={store}>
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
-    </Provider>
-  </React.StrictMode>
+    </PersistGate>
+  </Provider>
 );
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit';
 import dogsReducer from './dogsSlice';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, dogsReducer);
 
 export const store = configureStore({
   reducer: {
-    dogs: dogsReducer,
+    dogs: persistedReducer,
   },
 });
 
+export const persistor = persistStore(store);
+
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
